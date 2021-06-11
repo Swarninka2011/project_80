9fcf4fbe585d1799f703a603c5de2428e1f40cac
Swarninka2011 <79652086+Swarninka2011@users.noreply.github.com>
2021-06-11 19:22:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function submit()
+{
+var name_of_guest = document.getElementById("guest_name").value;
+console.log("name_of_guest");
+name_of_guest_array.push("name_of_guest");
+}
+function show()
+{
+console.log("name_of_guest")
+}
